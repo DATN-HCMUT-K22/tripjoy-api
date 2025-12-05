8504dda6b909839ded21a3f2bb9424e565306903
dinhduc0794 <ngdduc79@gmail.com>
2025-12-06 03:35:23 +0700

    refactor: externalize configuration and remove hardcoded secrets

--- src/main/java/com/tripjoy/api/TripjoyApiApplication.java
+++ src/main/java/com/tripjoy/api/TripjoyApiApplication.java
@@ -1,5 +1,6 @@
 package com.tripjoy.api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -9,6 +10,18 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class TripjoyApiApplication {
 
 	public static void main(String[] args) {
+
+		// Load file .env
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./") // Tìm file .env ở thư mục gốc dự án
+				.ignoreIfMissing() // Nếu không có file (VD: chạy trên Prod) thì bỏ qua, không lỗi
+				.load();
+
+		// Bơm các biến trong .env vào System Property của Java
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(TripjoyApiApplication.class, args);
 	}
 
