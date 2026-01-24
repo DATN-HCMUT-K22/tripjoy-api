257d086d63c9f35d8d9ad4cf2423e2a4b75eb72d
dinhduc0794 <ngdduc79@gmail.com>
2026-01-24 15:43:31 +0700

    fix: getMyConversation load full members instead of current user

--- src/main/java/com/tripjoy/api/repository/ConversationRepository.java
+++ src/main/java/com/tripjoy/api/repository/ConversationRepository.java
@@ -14,11 +14,19 @@ public interface ConversationRepository extends JpaRepository<Conversation, UUID
 
         @Query("SELECT DISTINCT c FROM Conversation c " +
                         "LEFT JOIN FETCH c.members m " +
-                        "LEFT JOIN FETCH m.user " +
+                        "LEFT JOIN FETCH m.user u " +
                         "LEFT JOIN FETCH c.group g " +
-                        "JOIN GroupMember gm ON gm.group.id = g.id AND gm.user.id = m.user.id " +
-                        "WHERE m.user.id = :userId " +
+                        "WHERE EXISTS (" +
+                        "  SELECT 1 FROM ConversationMember cm " +
+                        "  WHERE cm.conversation.id = c.id " +
+                        "  AND cm.user.id = :userId" +
+                        ") " +
                         "AND g.softDeleteInfo.isDeleted = false " +
-                        "AND gm.softDeleteInfo.isDeleted = false")
+                        "AND EXISTS (" +
+                        "  SELECT 1 FROM GroupMember gm " +
+                        "  WHERE gm.group.id = g.id " +
+                        "  AND gm.user.id = u.id " +
+                        "  AND gm.softDeleteInfo.isDeleted = false" +
+                        ")")
         List<Conversation> findAllByUserId(@Param("userId") UUID userId);
 }
